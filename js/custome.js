35887ac3537049e453de4645bcf89b65e912a3cb
[amdadulhaquemelon] <[amdadulhaquemelon63566@gmail.com]>
2021-01-25 20:25:43 +0600

    first

---
+++ js/custome.js
@@ -0,0 +1,24 @@
+(function ($) {
+
+	//Schedule Box Tabs
+
+	// slider
+	$(".video_slider").slick({
+		autoplay: true,
+		dots: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		centerMode: true,
+	});
+
+
+
+
+
+
+
+
+
+})(window.jQuery);
